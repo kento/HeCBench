4a335115acac5963423e946e62313ef5145d9f74
KOGA Junichiro <jkoga@vinas.com>
2025-02-07 13:52:13 +0900

    forgot to add the modified file for the rowwiseMoments-cuda benchmark

--- src2/rowwiseMoments-cuda/main.cu
+++ src2/rowwiseMoments-cuda/main.cu
@@ -19,7 +19,8 @@ void RowwiseMomentsKernel(
   using T_ACC = T;
   using WelfordType = WelfordData<T_ACC, int64_t>;
   using WelfordOp =
-    WelfordOps<T_ACC, T_ACC, int64_t, thrust::pair<T_ACC, T_ACC>>;
+//    WelfordOps<T_ACC, T_ACC, int64_t, thrust::pair<T_ACC, T_ACC>>;
+    WelfordOps<T_ACC, T_ACC, int64_t, cuda::std::pair<T_ACC, T_ACC>>;
 
   const int64_t i = blockIdx.x;
   WelfordOp welford_op = {/*correction=*/0, /*take_sqrt=*/false};
